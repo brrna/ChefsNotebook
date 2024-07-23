7f3523374b105a375b1b454d7bcded5e51d531c9
brrna <bern.varol@gmail.com>
2024-07-23 22:34:38 +0300

    SplashScreen designed

--- pages/Splash.js
+++ pages/Splash.js
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
+import { useNavigation } from '@react-navigation/native'
 
 const Splash = () => {
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate("CategoriesScreen")
+    }, 2000)
+  }, [])
+
   return (
-    <View>
-      <Text>Splash</Text>
-    </View>
+    <SafeAreaView style={styles.container} >
+      <Image 
+        style={styles.image}
+        source={require("../assets/images/logo.jpeg")} />
+    </SafeAreaView>
   )
 }
 
 export default Splash
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#6eaa86",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  image: {
+    height: hp(40),
+    width: wp(60)
+  }
+})
