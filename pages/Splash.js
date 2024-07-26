a4a4984237536774692d0bc50ae0219c7846983f
brrna <bern.varol@gmail.com>
2024-07-26 21:02:17 +0300

    font added

--- pages/Splash.js
+++ pages/Splash.js
@@ -2,22 +2,37 @@ import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React, { useEffect } from 'react'
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen'
 import { useNavigation } from '@react-navigation/native'
+import { useFonts, AguafinaScript_400Regular } from '@expo-google-fonts/aguafina-script';
 
 const Splash = () => {
 
   const navigation = useNavigation();
 
+  const [fontsLoaded] = useFonts({
+    AguafinaScript_400Regular,
+  });
+
   useEffect(() => {
-    setTimeout(() => {
-      navigation.navigate("CategoriesScreen")
-    }, 2000)
-  }, [])
+    const timer = setTimeout(() => {
+      navigation.navigate("CategoriesScreen");
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [navigation]);
+
+  if (!fontsLoaded) {
+    return null; // Wait for the fonts to load
+  }
 
   return (
     <SafeAreaView style={styles.container} >
-      <Image 
-        style={styles.image}
-        source={require("../assets/images/logo.jpeg")} />
+      <View style={styles.view} >
+        <Image
+          style={styles.image}
+          source={require("../assets/images/logo.jpeg")} />
+        <Text style={styles.text}>
+          chef's notebook
+        </Text>
+      </View>
     </SafeAreaView>
   )
 }
@@ -31,8 +46,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center"
   },
+  view: {
+    marginBottom: hp(10)
+  },
   image: {
     height: hp(40),
     width: wp(60)
+  },
+  text: {
+    fontFamily: "AguafinaScript_400Regular",
+    fontSize: hp(6)
   }
 })
