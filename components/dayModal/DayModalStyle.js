61207562f3b121e3d625ae7240e7559f3d578d6e
brrna <bern.varol@gmail.com>
2024-08-08 18:13:29 +0300

    DayModal component styled

--- components/dayModal/DayModalStyle.js
+++ components/dayModal/DayModalStyle.js
@@ -1,23 +1,24 @@
 import { StyleSheet } from "react-native";
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from "react-native-responsive-screen";
 
-export default function createdStyle(color) {
+export default function createdStyle(color, height, marginTop) {
     return StyleSheet.create({
         container: {
             flex: 1,
             backgroundColor: color.cream,
-            alignItems: "center",
+        },
+        scrollview: {
+            backgroundColor: color.cream,
         },
         imageView: {
             backgroundColor: color.cream,
-            height: hp(55)
+            height: height,
         },
         imageTopView: {
             backgroundColor: color.navyBlue,
-            height: hp(33),
-            width: wp(150),
-            borderBottomLeftRadius: wp(72),
-            borderBottomRightRadius: wp(72),
+            height: hp(38),
+            width: wp(100),
+            borderBottomLeftRadius: wp(75),
             alignItems: "center"
         },
         image: {
@@ -26,12 +27,11 @@ export default function createdStyle(color) {
             backgroundColor: "yellow",
             borderRadius: 100000000,
             position: "absolute",
-            marginTop: wp(30)
+            marginTop: marginTop
         },
         contentView: {
             backgroundColor: "white",
             width: wp(100),
-            height: hp(45),
             borderTopLeftRadius: hp(5),
             borderTopRightRadius: hp(5)
         }
