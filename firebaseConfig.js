9d835ef099be181616d4c3968a60ff21c73270b8
brrna <bern.varol@gmail.com>
2024-08-22 13:12:47 +0300

    firestore edited

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage"
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyAhPjB0nVneOR4f0xez51tQ5brgXZa2pKw",
@@ -16,4 +17,5 @@ const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
 
+export const db = getFirestore(app)
 export default app
--- pages/Notebook.js
+++ pages/Notebook.js
@@ -1,10 +1,47 @@
 import { SafeAreaView, StyleSheet, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { collection, addDoc, getDocs } from 'firebase/firestore'
+import MyButton from "../components/MyButton/myButton"
+import { db } from '../firebaseConfig'
 
 const Notebook = () => {
+
+  const [data, setData] = useState([]);
+  console.log("data: ", data)
+
+  const sendData = async() => {
+    try {
+      const docRef = await addDoc(collection(db, "ReactNativeLesson"), {
+        title: "zero to hero",
+        content: "react native",
+        lesson: 17
+      });
+      console.log("document written with ID: ", docRef.id);
+    } catch (e) {
+      console.log("error adding document: ", e)
+    }
+  }
+
+  const getData = async() => {
+    const querySnapshot = await getDocs(collection(db, "ReactNativeLesson"));
+    querySnapshot.forEach((doc) => {
+      //console.log(`${doc.id} => ${doc.data()} `);
+      setData(doc.data())
+      console.log("clicked")
+    })
+  }
+
   return (
     <SafeAreaView style={styles.container} >
-        <Text>Notebook</Text>
+        <MyButton 
+          text="post"
+          buttonText="kaydet"
+          onPress={sendData} />
+
+        <MyButton 
+          text="get"
+          buttonText="getir"
+          onPress={getData} />  
     </SafeAreaView>
   )
 }
@@ -14,7 +51,7 @@ export default Notebook
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "red",
+        backgroundColor: "lightblue",
         justifyContent: "center",
         alignItems: "center"
     }
